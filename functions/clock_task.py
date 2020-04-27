1186b7c0a00ba3478e6fd46548053808a1b0167a
evgeniy <ekulagin59@gmail.com>
2020-04-27 23:35:05 +0500

    task complete

--- functions/clock_task.py
+++ functions/clock_task.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 
 def clock_task(ssh, ntp_server):
@@ -9,13 +10,15 @@ def clock_task(ssh, ntp_server):
     if check_ntp_avail(ssh, ntp_server):
         config_commands.append("ntp server " + ntp_server + " prefer")
 
-    regex = re.compile(r"(.+),.+")
+    regex = re.compile(r"Clock is synchronized,.+")
 
     ssh.send_config_set(config_commands)
 
+    time.sleep(5)
+
     cmd_output = ssh.send_command('show ntp status')
 
-    match = regex.match(cmd_output)
+    match = regex.search(cmd_output)
 
     if match:
         return "Clock is sync"
